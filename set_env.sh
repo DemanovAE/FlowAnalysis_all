a22796661b24d425e30c33a869fc37db9583e717
DemanovAE <demanov1997@gmail.com>
2023-03-21 15:05:25 +0300

    code change

--- set_env.sh
+++ set_env.sh
@@ -2,5 +2,5 @@
 
 #export ST_FEMTO_DST_INC_DIR=/scratch2/$USER/STAR/BES/StFemtoEvent
 #export ST_FEMTO_DST_INC_DIR=$HOME/STAR_BES/StFemtoEvent
-export ST_FEMTO_DST_INC_DIR=/home/aleksandr/STAR_BES/StFemtoEvent
+export ST_FEMTO_DST_INC_DIR=$PWD/StFemtoEvent
 export ST_FEMTO_DST_BUILD_DIR=$ST_FEMTO_DST_INC_DIR
